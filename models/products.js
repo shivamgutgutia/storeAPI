7a53c104068beaf9a798d819e978f9a84a653c38
Shivam Gutgutia <gutgutia159@gmail.com>
2023-10-13 16:16:17 +0530

    Implemented db schema

---
+++ models/products.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter a product name"]
+    },
+
+    price:{
+        type:Number,
+        required:[true,"Please enter a product price"]
+    },
+
+    features:{
+        type:Boolean,
+        default:false
+    },
+
+    rating:{
+        type:Number,
+        default:4
+    },
+
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+
+    company:{
+        type:String,
+        enum:{
+            values:["ikea","Liddy","caressa","marcos",""],
+            message:"Please enter a valid value"
+        }
+    }
+
+
+})
+
+module.exports = mongoose.model("Product",productSchema)
