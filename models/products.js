57b78195976b4793d88aa42a460f58d1f1c98218
Shivam Gutgutia <gutgutia159@gmail.com>
2023-10-13 19:07:18 +0530

    Ficed a spelling error in model schema

--- models/products.js
+++ models/products.js
@@ -11,7 +11,7 @@ const productSchema = new mongoose.Schema({
         required:[true,"Please enter a product price"]
     },
 
-    features:{
+    featured:{
         type:Boolean,
         default:false
     },
